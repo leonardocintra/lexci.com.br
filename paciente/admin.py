6ef1ea195ca5317117e07d7d2fab0b50d8488e7a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-14 11:48:42 -0200

    admin register Paciente

--- paciente/admin.py
+++ paciente/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Paciente
 
-# Register your models here.
+
+class PacienteAdmin(admin.ModelAdmin):
+    fields = ('nome', 'cartao_sus', )
+
+admin.site.register(Paciente, PacienteAdmin)
