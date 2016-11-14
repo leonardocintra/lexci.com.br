6f5ad1e07d67fd68b2026e84f4bc73ba59d35ba9
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-14 17:06:11 -0200

    add about page

--- core/views.py
+++ core/views.py
@@ -4,4 +4,9 @@ class IndexView(TemplateView):
     template_name = 'core/index.html'
 
 
+class AboutView(TemplateView):
+    template_name = 'core/about.html'
+
+
 index = IndexView.as_view()
+about = AboutView.as_view()
--- lexci/urls.py
+++ lexci/urls.py
@@ -4,5 +4,6 @@ from core import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^quem-somos/$', views.about, name='about'),
     url(r'^admin/', admin.site.urls),
 ]
