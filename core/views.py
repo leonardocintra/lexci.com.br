13a6fe5722caa359ac22f422994ba18028314d8b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-18 15:56:38 -0200

    retirado botoes do index e page contact

--- core/views.py
+++ core/views.py
@@ -7,10 +7,16 @@ class IndexView(TemplateView):
 class AboutView(TemplateView):
     template_name = 'core/about.html'
 
+
+class ContactView(TemplateView):
+    template_name = 'core/contact.html'
+
+
 class ManagementView(TemplateView):
     template_name = 'core/management.html'
 
 
 index = IndexView.as_view()
 about = AboutView.as_view()
+contact = ContactView.as_view()
 management = ManagementView.as_view()
--- lexci/urls.py
+++ lexci/urls.py
@@ -5,6 +5,7 @@ from core import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^quem-somos/$', views.about, name='about'),
+    url(r'^contato/$', views.contact, name='contact'),
     url(r'^administrativo/$', views.management, name='management'),
     url(r'^admin/', admin.site.urls),
 ]
