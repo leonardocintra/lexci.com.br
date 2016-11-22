d19e7aa4b80725ca126a20f9f10d3c698d009f71
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-21 22:55:59 -0200

    criado a view list paciente

--- paciente/views.py
+++ paciente/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Paciente
 
-# Create your views here.
+
+class ListPaciente(ListView):
+    model = Paciente
+    template_name = 'paciente/paciente_list.html'
+    context_object_name = 'paciente_list'
+
+
+paciente_list = ListPaciente.as_view()
