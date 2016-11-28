1ba1df31bb11d87ae01544ba47cee59bbbd1225d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-11-28 14:01:47 -0200

    deu certo o create paciente com endereco

--- paciente/forms.py
+++ paciente/forms.py
@@ -1,13 +1,24 @@
-from django.forms import ModelForm
+from django import forms
 from django.forms.models import inlineformset_factory
-
 from .models import Paciente, PacienteEndereco
 
+class PacienteForm(forms.ModelForm):
 
-class PacienteForm(ModelForm):
-    
     class Meta:
         model = Paciente
+        exclude = ('data_cadastro', 'data_atualizacao', )
+
+class EnderecoPacienteForm(forms.ModelForm):
+
+    class Meta:
+        model = PacienteEndereco
+        fields = ('logradouro', 'numero_casa', 'complemento', 'bairro', 'uf', 'codigo_municipio', 'municipio', 'cep', 'fone_ddd', 'fone_numero', 'ponto_de_referencia')
 
 
-EnderecoPacienteFormSet = inlineformset_factory(Paciente, PacienteEndereco)
+EnderecoFormSet = inlineformset_factory(
+    Paciente, 
+    PacienteEndereco,
+    can_delete=False,
+    fields=('__all__'),
+    extra=1
+)
--- paciente/views.py
+++ paciente/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.db import transaction
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, CreateView
 
 from .models import Paciente
-from .forms import PacienteForm, EnderecoPacienteFormSet
+from .forms import PacienteForm, EnderecoFormSet
 
 
 class ListPaciente(ListView):
@@ -14,13 +15,41 @@ class ListPaciente(ListView):
 
 
 class CreatePaciente(CreateView):
-    model = Paciente
-    template_name = 'paciente_endereco_new.html'
     form_class = PacienteForm
-    success_url = reverse_lazy('paciente:paciente_list')
+    success_url = 'paciente:paciente_list'
+    model = Paciente
 
     def get(self, request, *args, **kwargs):
-        pass
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        endereco_form = EnderecoFormSet
+        return self.render_to_response(self.get_context_data(form=form, endereco_form=endereco_form))
+    
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        endereco_form = EnderecoFormSet(self.request.POST)
+        if (form.is_valid() and endereco_form.is_valid()):
+            return self.form_valid(form, endereco_form)
+        else:
+            return self.form_invalid(form, endereco_form)
+        
+    def form_valid(self, form, endereco_form):
+        self.object = form.save()
+        endereco_form.instance = self.object
+        endereco_form.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def form_invalid(self, form, endereco_form):
+        return self.render_to_response(
+            self.get_context_data(form=form, endereco_form=endereco_form)
+        )
+    
+    def get_context_data(self, **kwargs):
+        kwargs.update({})
+        return kwargs
 
 
 
