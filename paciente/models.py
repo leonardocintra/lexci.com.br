31534bca235b797a8f44f5382ea312726b7c9157
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-01 07:41:43 -0200

    remodelando o PacienteEndereco model

--- paciente/models.py
+++ paciente/models.py
@@ -41,17 +41,17 @@ class Paciente(models.Model):
 
 class PacienteEndereco(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    logradouro = models.CharField(max_length=100, blank=True, null=True)
-    numero_casa = models.CharField('Nº casa',max_length=10, blank=True, null=True)
-    complemento = models.CharField(max_length=50, blank=True, null=True)
-    bairro = models.CharField(max_length=50, blank=True, null=True)
-    uf = models.CharField('UF', max_length=2, blank=True, null=True)
+    logradouro = models.CharField(max_length=100, blank=True, default='')
+    numero_casa = models.CharField('Nº casa',max_length=10, blank=True, default='')
+    complemento = models.CharField(max_length=50, blank=True, default='')
+    bairro = models.CharField(max_length=50, blank=True, default='')
+    uf = models.CharField('UF', max_length=2, blank=True, default='')
     codigo_municipio = models.IntegerField('Codigo Municipio', blank=True, null=True)
-    municipio = models.CharField(max_length=100, blank=True, null=True)
-    cep = models.CharField('CEP', max_length=8, blank=True, null=True)
-    fone_ddd = models.CharField('DDD:', max_length=2, blank=True, null=True)
-    fone_numero = models.CharField('Telefone:', max_length=11, blank=True, null=True)
-    ponto_de_referencia = models.CharField(max_length=100, blank=True, null=True)
+    municipio = models.CharField(max_length=100, blank=True, default='')
+    cep = models.CharField('CEP', max_length=8, blank=True, default='')
+    fone_ddd = models.CharField('DDD:', max_length=2, blank=True, default='')
+    fone_numero = models.CharField('Telefone:', max_length=11, blank=True, default='')
+    ponto_de_referencia = models.CharField(max_length=100, blank=True, default='')
 
     def __str__(self):
         return self.paciente.nome + ' - ' + self.municipio
