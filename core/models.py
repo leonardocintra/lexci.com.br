32c05bea9c2a0e4e6283a839feb91445cc4a12ce
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-05 12:38:11 -0200

    create mode convenio

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Convenio(models.Model):
+    descricao = models.CharField(max_lengt=100)
+    slug = models.SlugField('Identificador', max_length=100)
+
+    class Meta:
+        verbose_name = 'Convenio'
+        verbose_name = 'Convenios'
+        ordering = ['descricao']
+    
+    def __str__ (self):
+        return self.descricao
