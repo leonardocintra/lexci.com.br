c7ffad29287ccb6c4eb106618e6fdc07a031128f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-14 09:09:46 -0200

    alterando informacoes obrigatorias no paciente

--- paciente/models.py
+++ paciente/models.py
@@ -7,11 +7,11 @@ from .constants import RACA
 
 class Paciente(models.Model):
     nome = models.CharField(max_length=200)
-    cartao_sus = models.CharField('SUS', unique=True, max_length=15)
-    nome_mae = models.CharField('Nome da mãe', max_length=150)
+    cartao_sus = models.CharField('SUS', max_length=15, blank=True, null=True)
+    nome_mae = models.CharField('Nome da mãe', max_length=150, blank=True, null=True)
     apelido = models.CharField(max_length=100, blank=True, null=True)
     cpf = models.CharField('CPF', unique=True, max_length=11)
-    nacionalidade = models.CharField(max_length=100, default='brasileira')
+    nacionalidade = models.CharField(max_length=100, default='brasileira', blank=True, null=True)
     data_nascimento = models.DateField()
     raca = models.CharField('Raça', choices=RACA, default='BRA', max_length=3)
     data_cadastro = models.DateTimeField(auto_now_add=True)
@@ -32,7 +32,7 @@ class Paciente(models.Model):
         try: 
             birthday = data_nascimento.replace(year=today.year)
         except ValueError: # raised when birth date is February 29 and the current year is not a leap year
-            birthday = data_nascimento.replace(year=today.year, month=data_nascimento.month+1, day=1)
+            birthday = data_nascimento.replace(year=today.year, month=data_nascimento.month + 1, day=1)
         if birthday > today:
             return today.year - data_nascimento.year - 1
         else:
@@ -41,16 +41,16 @@ class Paciente(models.Model):
 
 class PacienteEndereco(models.Model):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
-    logradouro = models.CharField(max_length=100, blank=True, default='')
+    logradouro = models.CharField(max_length=100)
     numero_casa = models.CharField('Nº casa',max_length=10, blank=True, default='')
     complemento = models.CharField(max_length=50, blank=True, default='')
     bairro = models.CharField(max_length=50, blank=True, default='')
-    uf = models.CharField('UF', max_length=2, blank=True, default='')
+    uf = models.CharField('UF', max_length=2)
     codigo_municipio = models.IntegerField('Codigo Municipio', blank=True, null=True)
-    municipio = models.CharField(max_length=100, blank=True, default='')
+    municipio = models.CharField(max_length=100)
     cep = models.CharField('CEP', max_length=8, blank=True, default='')
     fone_ddd = models.CharField('DDD:', max_length=2, blank=True, default='')
-    fone_numero = models.CharField('Telefone:', max_length=11, blank=True, default='')
+    fone_numero = models.CharField('Telefone:', max_length=11)
     email = models.EmailField(blank=True, default='')
     ponto_de_referencia = models.CharField(max_length=100, blank=True, default='')
 
