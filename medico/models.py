172ad2411939344b4576d75ceaab80af2192b63d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-12-15 10:15:38 -0200

    crm e telefone nao sao obritatorios

--- medico/models.py
+++ medico/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Medico(models.Model):
     nome = models.CharField(max_length=200)
-    crm = models.CharField('CRM', max_length=50)
-    telefone = models.CharField('Telefone:', max_length=20)
+    crm = models.CharField('CRM', max_length=50, blank=True, null=True)
+    telefone = models.CharField('Telefone:', max_length=20, blank=True, null=True)
     data_cadastro = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
 
