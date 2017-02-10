02c88beefcbc33d033b454a18340404486a358b5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-10 19:43:01 -0200

    criando app exame

--- laudo/models.py
+++ laudo/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+from paciente.models import Paciente
+from medico.models import Medico
+from core.models import Convenio
+
+
+class Laudo(models.Model):
+    pass
+    #paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='paciente')
+    #medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name='medico')
+    #convenio = models.ForeignKey(Convenio, on_delete=models.CASCADE, related_name='convenio')
