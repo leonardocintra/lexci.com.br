301fd2cd934c14d6fbd1ce0cad19fa7664ffbdba
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-10 18:48:52 -0200

    desativando envio de email

--- core/forms.py
+++ core/forms.py
@@ -10,9 +10,10 @@ class ContactForm(forms.Form):
     message = forms.CharField(label='Mensagem', widget=forms.Textarea(attrs={'class': 'form-control'}), max_length=300 )
 
     def send_mail(self):
-        recipient = [settings.EMAIL_HOST_USER]
-        sender = self.cleaned_data['email']
-        subject = "CONTATO - " + self.cleaned_data['name']
-        message = " TELEFONE: " + self.cleaned_data['phone'] + "\n MENSAGEM:" + self.cleaned_data['message'] + "\n EMAIL: " + self.cleaned_data['email']
-        print("Contact: email sent to: " + sender)
-        send_mail(subject, message, sender, recipient)
+        #recipient = [settings.EMAIL_HOST_USER]
+        #sender = self.cleaned_data['email']
+        #subject = "CONTATO - " + self.cleaned_data['name']
+        #message = " TELEFONE: " + self.cleaned_data['phone'] + "\n MENSAGEM:" + self.cleaned_data['message'] + "\n EMAIL: " + self.cleaned_data['email']
+        #print("Contact: email sent to: " + sender)
+        #send_mail(subject, message, sender, recipient)
+        pass
