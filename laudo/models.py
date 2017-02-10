c4dfdc2953c4bac85d0331bb8bd0b7c34f21029f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-10 19:44:49 -0200

    apagando eapp exame pq nao vai usar

--- laudo/models.py
+++ laudo/models.py
@@ -7,6 +7,3 @@ from core.models import Convenio
 
 class Laudo(models.Model):
     pass
-    #paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='paciente')
-    #medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name='medico')
-    #convenio = models.ForeignKey(Convenio, on_delete=models.CASCADE, related_name='convenio')
