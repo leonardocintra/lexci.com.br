ede9d03c68fa3e7b04841a6a6422c7cd74f0a56d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-10 21:08:20 -0200

    mais ou menos o admin laudo

--- laudo/admin.py
+++ laudo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Exame, ItemExame, Laudo
 
-# Register your models here.
+
+class ExameAdmin(admin.ModelAdmin):
+    fields = ('descricao', )
+
+
+class ItemExameAdmin(admin.ModelAdmin):
+    fields = ('exame', 'descricao_item', )
+
+class LaudoAdmin(admin.ModelAdmin):
+    fields = ('paciente', 'medico', 'convenio', 'exames', )
+
+
+admin.site.register(Exame, ExameAdmin)
+admin.site.register(ItemExame, ItemExameAdmin)
+admin.site.register(Laudo, LaudoAdmin)
