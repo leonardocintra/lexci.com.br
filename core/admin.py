7b9b5894503ed0290f7ecc48def5f38ea0cefdea
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-11 12:02:12 -0200

    recadastrando admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
+"""
+Admin Core
+
+"""
 from django.contrib import admin
 from .models import Convenio
 
 class ConvenioAdmin(admin.ModelAdmin):
+    """ Cadastro manual de convenio """
     fields = ('descricao', 'slug')
+    prepopulated_fields = {'slug': ('descricao', )}
 
 
 admin.site.register(Convenio, ConvenioAdmin)
