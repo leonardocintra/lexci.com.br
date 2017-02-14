41a4524673cdd547a2051b31a434d313052730a6
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-02-14 21:43:31 -0200

    cadastro e update do sexo

--- paciente/views.py
+++ paciente/views.py
@@ -72,7 +72,7 @@ class CreatePaciente(CreateView):
 class UpdatePaciente(UpdateView):
     model = Paciente
     template_name = 'paciente/paciente_update.html'
-    fields = ('nome', 'cartao_sus', 'nome_mae', 'apelido', 'cpf', 'nacionalidade', 'data_nascimento', 'raca', 'profissao', 'ativo')
+    fields = ('nome', 'cartao_sus', 'nome_mae', 'apelido', 'cpf', 'nacionalidade', 'data_nascimento', 'raca', 'sexo', 'profissao', 'ativo')
 
     def get_success_url(self):
         return reverse_lazy('paciente:paciente_detail', kwargs={'pk': self.object.pk})
