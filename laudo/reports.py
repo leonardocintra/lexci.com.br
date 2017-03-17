3f854e1ed76a5942e60797f2c7fc080f7060bee4
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-03-16 21:13:50 -0300

    decode nao rolou

--- laudo/reports.py
+++ laudo/reports.py
@@ -89,7 +89,7 @@ def write_paciente(canvas, paciente, medico):
     c.setFont('Helvetica', tamanho_letra)
     c.drawString(45, 720, 'Nome: ')
     c.setFont('Helvetica-Bold', tamanho_letra)
-    c.drawString(95, 720, p.nome.decode('utf-8'))
+    c.drawString(95, 720, p.nome)
 
     c.setFont('Helvetica', tamanho_letra)
     c.drawString(450, 720, 'Idade: ')
