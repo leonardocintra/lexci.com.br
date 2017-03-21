e60b0f26e6fdebe82c75a93a08a5d0c83be0312a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-03-21 20:37:53 -0300

    renomeando view medico

--- medico/views.py
+++ medico/views.py
@@ -7,13 +7,13 @@ from fm.views import AjaxCreateView, AjaxUpdateView
 from .models import Medico
 
 
-class ListMedicoView(ListView):
+class MedicoList(ListView):
     model = Medico
     template_name = 'medico/medico_list.html'
     context_object_name = 'medico_list'
 
 
-class CreateMedicoView(AjaxCreateView):
+class CreateMedico(AjaxCreateView):
     model = Medico
     fields = ['nome', 'crm', 'telefone', ]
 
@@ -21,15 +21,15 @@ class CreateMedicoView(AjaxCreateView):
         return reverse_lazy('medico:medico_list')
 
 
-class DetailMedicoView(DetailView):
+class MedicoDetail(DetailView):
     model = Medico
 
     def get_context_data(self, **kwargs):
-        context = super(DetailMedicoView, self).get_context_data(**kwargs)
+        context = super(MedicoDetail, self).get_context_data(**kwargs)
         return context
 
     
-class UpdateMedicoView(UpdateView):
+class MedicoUpdate(UpdateView):
     model = Medico
     fields = ['nome', 'crm', 'telefone']
 
@@ -37,13 +37,13 @@ class UpdateMedicoView(UpdateView):
         return reverse_lazy('medico:medico_list')
 
     def get_context_data(self, **kwargs):
-        context = super(UpdateMedicoView, self).get_context_data(**kwargs)
+        context = super(MedicoUpdate, self).get_context_data(**kwargs)
         context['pk'] = self.kwargs['pk']
         context['nome'] = get_object_or_404(Medico, pk=self.kwargs['pk'])
         return context
 
 
-class DeleteMedidoView(DeleteView):
+class MedicoDelete(DeleteView):
     model = Medico
 
     def get_success_url(self):
@@ -52,8 +52,8 @@ class DeleteMedidoView(DeleteView):
 
 
 
-medico_list = ListMedicoView.as_view()
-medico_create = CreateMedicoView.as_view()
-medico_detail = DetailMedicoView.as_view()
-medico_update = UpdateMedicoView.as_view()
-medico_delete = DeleteMedidoView.as_view()
+medico_list = MedicoList.as_view()
+medico_create = CreateMedico.as_view()
+medico_detail = MedicoDetail.as_view()
+medico_update = MedicoUpdate.as_view()
+medico_delete = MedicoDelete.as_view()
