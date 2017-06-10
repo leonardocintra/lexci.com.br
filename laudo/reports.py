d55fd0b7efdf9fc34e866ce673ea99b6be668e9d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-06-10 12:56:25 -0300

    alterando endereco do laudo

--- laudo/reports.py
+++ laudo/reports.py
@@ -124,17 +124,19 @@ def write_paciente(canvas, paciente, medico):
     return c
 
 def write_title(canvas):
+    organizador = 20
+
     c = canvas
     c.setFont('Helvetica-BoldOblique', 10)
     c.drawImage("core/static/images/logolexci.png", 90, 760, 120, 40)
-    c.drawString(350, 820, 'LABORATÓRIO DE EXAMES CITOLÓGICOS')
-    c.drawString(320, 805, 'Rua Coronel Timóteo 91 - Bairro Centro - Ibiraci - MG')
-    c.drawString(375, 790, '(35) 9.9991-8888 / (16) 99408-0682')
+    c.drawString(340 - organizador, 820, 'LABORATÓRIO DE EXAMES CITOLÓGICOS')
+    c.drawString(285 - organizador, 805, 'Rua Marechal Floriano Peixoto 295 – Bairro Centro – Ibiraci – MG')
+    c.drawString(360 - organizador, 790, '(35) 9.9991-8888 / (16) 99408-0682')
     c.setFont('Helvetica-Oblique', 8)
-    c.drawString(350, 775, 'e-mail: atendimento@lexci.com.br / marcio@lexci.com.br')
+    c.drawString(340 - organizador, 775, 'e-mail: atendimento@lexci.com.br / marcio@lexci.com.br')
     c.setFont('Helvetica-BoldOblique', 10)
-    c.drawString(355, 760, 'Responsável: Dr Márcio Gimenes França')
-    c.drawString(340, 745, 'CRBM 8803 Inscrição: 16/3453 CNES: 9088148')
+    c.drawString(345 - organizador, 760, 'Responsável: Dr Márcio Gimenes França')
+    c.drawString(330 - organizador, 745, 'CRBM 8803 Inscrição: 16/3453 CNES: 9088148')
     write_horizontal_line(c, 740)
     return c
 
