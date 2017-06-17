b009aadc22ac9bf21cc802511b44cf72c57e73a5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-06-17 13:04:21 -0300

    mostranto o tipo de exame

--- paciente/templatetags/paciente_extras.py
+++ paciente/templatetags/paciente_extras.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from laudo.views import laudo
 from paciente.models import Paciente
 
 register = Library()
@@ -6,3 +7,8 @@ register = Library()
 @register.assignment_tag
 def get_idade_paciente(data_nascimento):
     return Paciente.calcula_idade(data_nascimento)
+
+@register.simple_tag
+def get_tipo_exame(laudo_id):
+    tipo_laudo = laudo.get_tipo_exame_laudo(laudo_id)
+    return laudo.get_descricao_tipo_exame_laudo(tipo_laudo)
