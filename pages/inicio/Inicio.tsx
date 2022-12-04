e4a135932c74b34d8b57ad04b8c32234d98a573f
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2022-12-04 19:25:41 -0300

    fix: bg-cover

--- pages/inicio/Inicio.tsx
+++ pages/inicio/Inicio.tsx
@@ -4,7 +4,7 @@ import Worklab from '../worklab/Worklab';
 export default function Inicio() {
     return (
         <div>
-            <div className='bg-[url("/img/lab1.png")] h-96 w-full p-24'>
+            <div className='bg-[url("/img/lab1.png")] h-96 w-full p-24 bg-cover'>
                 <Image src={'/img/logolexci.png'} alt='Logo Lexci' width={290} height={200} />
             </div>
 
